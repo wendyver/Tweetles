0c771d9c75f3656b9f90c280224b882753eb7917
Wendy Veronie <ichigo.milk@me.com>
2024-06-12 13:50:10 -0500

    displays new tweets and timestamp

--- index.js
+++ index.js
@@ -1,8 +1,41 @@
 
 $(document).ready(() => {
   const $body = $('body');
-  $body.html('');
+  // separate container div for tweets, appended to the body
+  const $tweetContainer = $('<div id="tweet-container"></div>');
+  $body.append($tweetContainer);
+ // $body.html(''); // => clears body (or any tag)
 
+ //function creates a tweet element
+ const createTweetElement = (tweet) => {
+  const $tweet = $('<div class="tweet"></div>');
+  const text = `@${tweet.user}: ${tweet.message}`;
+  const timestamp = moment(tweet.created_at).fromNow(); // format time stamp
+
+  $tweet.text(text);
+  $tweet.append(`<span class="timestamp">(${timestamp})</span>`); // display time stamp
+
+  return $tweet;
+ };
+
+ // function for showing new tweets
+ const showNewTweets = () => {
+  const latestTweets = streams.home.slice(-4); // Get the 4 latest tweets
+  latestTweets.forEach((tweet) => {
+    const $tweet = createTweetElement(tweet);
+    $tweetContainer.prepend($tweet); // Prepend new tweets to the top
+  });
+ };
+
+ // first new tweet display
+ showNewTweets();
+
+ //Keep updating UI with new tweets
+ setInterval(() => {
+  showNewTweets();
+ }, 7000); // updates every 7 seconds
+// make a function for this
+/*
   const $tweets = streams.home.map((tweet) => {
     const $tweet = $('<div></div>');
     const text = `@${tweet.user}: ${tweet.message}`;
@@ -12,5 +45,5 @@ $(document).ready(() => {
     return $tweet;
   });
   $body.append($tweets);
-
+*/
 });
