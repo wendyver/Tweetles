3cc04ebc5fc498d7c4ceeca732468e44f2ec2da0
Wendy Veronie <ichigo.milk@me.com>
2024-06-18 16:46:54 -0500

    kinda done

--- index.js
+++ index.js
@@ -37,14 +37,12 @@ $(document).ready(() => {
 
 $('<style>').text(style).appendTo('head');
 
+// append form to tweet container
 $newTweetForm.append($newTweetInput).append($submitButton);
-$body.append($tweetContainer); // append tweet container to body
- $tweetContainer.append($newTweetForm); // append form to tweet container
+$body.append($tweetContainer).append($newTweetForm);
 
- // moment function
-const formatTimestamp = (timestamp) => {
-  return moment(timestamp).fromNow();
-}
+
+
 
  //function creates a tweet element
  const createTweetElement = (tweet) => {
@@ -56,52 +54,84 @@ const formatTimestamp = (timestamp) => {
   const $timestamp =$('<span class="timestamp"></span>').text(` (${formatTimestamp(tweet.created_at)})`); // format time stamp
 
   $tweet.append($username).append($message).append($timestamp); // display time stamp
-
   return $tweet;
  };
 
+  // moment function to format time stamp
+const formatTimestamp = (timestamp) => {
+  return moment(timestamp).fromNow();
+  };
 
- // function for showing new tweets
+ // modified show new tweets function
+ // let lastDisplayedTweetIndex = 0;
+
+// show new tweets function
  const showNewTweets = () => {
-  $tweetContainer.find('.tweet').remove();
+  $tweetContainer.empty(); // clear existing tweets
+  /*
+  // get new tweets since the last displayed index
+  const newTweets = streams.home.slice(lastDisplayedTweetIndex);
+// update lastDTI to the latest tweet
+  lastDisplayedTweetIndex = streams.home.length;
+*/
+  //prepend new tweets to the top of the tweet container
+ // newTweets.reverse().forEach((tweet) => {
   streams.home.forEach((tweet) => {
     const $tweet = createTweetElement(tweet);
-    $tweetContainer.append($tweet); // Append new tweets to the top
+    $tweetContainer.prepend($tweet); // prepend new tweet to the top
+  });
+};
+
+  //$tweetContainer.find('.tweet').remove();
+ /*
+  reversedTweets.forEach((tweet) => {
+    const $tweet = createTweetElement(tweet);
+    $tweetContainer.prepend($tweet); // prepend new tweets to the top
   });
  };
-
- //add new tweets to streams
+*/
+ //add new tweets to streams function
 const addNewTweet = (text) => {
   const newTweet = {
     user: "YOU",
     message: text,
     created_at: new Date()
   };
-  streams.home.push(newTweet);
+  streams.home.unshift(newTweet); // add new tweets to the beginning of streams.home
+
   if (!streams.users['YOU']) {
     streams.users['YOU'] = [];
   }
   streams.users['YOU'].push(newTweet);
-  showNewTweets();
+
+ // show the new tweet immediately at the top
+ const $tweet = createTweetElement(newTweet);
+ $tweetContainer.prepend($tweet);
+
+ $newTweetInput.val('');
+
+ // showNewTweets(); // show the new tweet immediately
 };
 
  // user timeline function
 const showUserTimeLine = (username) => {
-  $tweetContainer.find('.tweet').remove();
+  $tweetContainer.empty(); // clear existing tweets
   streams.users[username].forEach((tweet) => {
     const $tweet = createTweetElement(tweet);
-    $tweetContainer.append($tweet);
+    $tweetContainer.append($tweet); // append user's tweets to the tweet container
   });
 };
 
- showNewTweets();
+ showNewTweets(); // initially show existing tweets
 
  // new tweet event listener
  $newTweetForm.on('submit', (event) => {
   event.preventDefault();
-  const newTweetText = $newTweetInput.val();
-  addNewTweet(newTweetText);
-  $newTweetInput.val('') // clear input after submitting
+  const newTweetText = $newTweetInput.val().trim();
+  if (newTweetText !== '') {
+  addNewTweet(newTweetText); // call addNewTweet with the text
+  // $newTweetInput.val('') // clear input after submitting
+  }
 });
 
 //Keep updating UI with new tweets
@@ -109,30 +139,8 @@ setInterval(() => {
   showNewTweets();
  }, 7000); // updates every 7 seconds
 
-
-
-
- 
-
-
-
-
-  
-  
-
-  
 });
-/*
-  const $tweets = streams.home.map((tweet) => {
-    const $tweet = $('<div></div>');
-    const text = `@${tweet.user}: ${tweet.message}`;
 
-    $tweet.text(text);
-
-    return $tweet;
-  });
-  $body.append($tweets);
-*/
 
 
 
